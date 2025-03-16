73c18f42e82908d61cfb4dd5502ea88fb6a86065
applefather-eth <applefather.eth@gmail.com>
2025-03-16 13:02:56 +0700

    hide wallet login option

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,28 @@
-"use client";
+'use client'
 
-import React, { ReactNode } from 'react';
-import { useAccount, WagmiProvider } from 'wagmi';
+import React, { ReactNode } from 'react'
+import { useAccount, WagmiProvider } from 'wagmi'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { config } from './config';
-import { WalletOptions } from './wallet-options';
-import { Account } from './account';
+import { config } from './config'
+import { WalletOptions } from './wallet-options'
+import { Account } from './account'
 
-function ConnectWallet() {
-    const { isConnected } = useAccount()
-    if (isConnected) return <Account />
-    return <WalletOptions />
-  }
+// Commented out wallet connection UI
+/*function ConnectWallet() {
+	const { isConnected } = useAccount()
+	if (isConnected) return <Account />
+	return <WalletOptions />
+}*/
+
 export default function App({ children }: { children: ReactNode }) {
-  const queryClient = new QueryClient()
-  
-  return (
-    <QueryClientProvider client={queryClient}>
-      <WagmiProvider config={config}>
-        <ConnectWallet />
-        {children}
-      </WagmiProvider>
-    </QueryClientProvider>
-  );
-} 
+	const queryClient = new QueryClient()
+
+	return (
+		<QueryClientProvider client={queryClient}>
+			<WagmiProvider config={config}>
+				{/* <ConnectWallet /> */}
+				{children}
+			</WagmiProvider>
+		</QueryClientProvider>
+	)
+}
